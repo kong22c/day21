01efc879e9c328d1b11da7ea20b541a5985d850b
kong22c <2694262371@qq.com>
2020-05-29 12:18:55 +0800

    修改

--- app/src/main/java/com/example/day21/MainActivity.java
+++ app/src/main/java/com/example/day21/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        double c=3;
+        String d="我们";
     }
 }
